097222b44b69d79be00a1bfdcc8444d27bb249d3
Satwika Vysetty <vysetty96@gmail.comgit config --global user.email vysetty96@gmail.com>
2021-03-15 18:31:06 +0530

    changed the name of the dataframe

--- src/data_management/task_create_data.py
+++ src/data_management/task_create_data.py
@@ -7,39 +7,36 @@ from src.config import SRC
 
 
 @pytask.mark.depends_on(SRC / "original_data" / "policy_data.csv")
-@pytask.mark.produces(BLD / "data" / "stringency_index_data.csv")
+@pytask.mark.produces(BLD / "data" / "covid_policy.csv")
 def task_create_data(depends_on, produces):
-    """Read the data"""
-    covid_policy = pd.read_csv(depends_on).set_index("indicator")
+    """Read the data
+    """
+    policy_data = pd.read_csv(depends_on).set_index("indicator")
     """Change datatypes
     """
     for col in ["start_date", "end_date"]:
-        covid_policy[col] = covid_policy[col].astype("datetime64[ns]")
+        policy_data[col] = policy_data[col].astype("datetime64[ns]")
     """Decompose data for every single day from 2020-02-01 to 2021-02-14 for each indicator
     """
-    covid_policy["row"] = range(len(covid_policy))
-    starts = covid_policy[
-        ["start_date", "scale", "flag", "recorded_flag", "maximum", "row"]
-    ].rename(columns={"start_date": "date"})
-    ends = covid_policy[
-        ["end_date", "scale", "flag", "recorded_flag", "maximum", "row"]
-    ].rename(columns={"end_date": "date"})
-    df_decomp = pd.concat([starts, ends]).set_index("row", append=True)
-    df_decomp = df_decomp.groupby(level=[0, 1]).apply(
-        lambda x: x.set_index("date").resample("D").fillna(method="pad")
-    )
-    df_decomp.reset_index(inplace=True)
+    policy_data["row"] = range(len(policy_data))
+    starts = policy_data[["start_date", "scale", "flag", "recorded_flag", 
+                          "maximum", "row"]].rename(columns={"start_date": "date"})
+    ends = policy_data[["end_date", "scale", "flag", "recorded_flag", 
+                         "maximum", "row"]].rename(columns={"end_date": "date"})
+    policy_data_decomp = pd.concat([starts, ends]).set_index("row", append=True)
+    policy_data_decomp = policy_data_decomp.groupby(level=[0, 1]).apply(
+        lambda x: x.set_index("date").resample("D").fillna(method="pad"))
+    policy_data_decomp.reset_index(inplace=True)
     """Long to wide format
     """
-    stringency_index = df_decomp.pivot(
+    covid_policy = policy_data_decomp.pivot(
         index="date",
         columns="indicator",
-        values=["scale", "flag", "recorded_flag", "maximum"],
-    )
-    stringency_index.columns = ["_".join(col) for col in stringency_index.columns]
+        values=["scale", "flag", "recorded_flag", "maximum"])
+    covid_policy.columns = ["_".join(col) for col in stringency_index.columns]
     """Fill missing values
     """
-    stringency_index.replace({-100: np.nan}, inplace=True)
+    covid_policy.replace({-100: np.nan}, inplace=True)
     """Save the data
     """
-    stringency_index.to_csv(produces)
+    covid_policy.to_csv(produces)
