42bbb2e3e8a8901c075ca3d87d49751c8265f6aa
Satwika Vysetty <vysetty96@gmail.comgit config --global user.email vysetty96@gmail.com>
2021-03-23 12:27:42 +0530

    added list comprehensions

--- src/analysis/task_create_index.py
+++ src/analysis/task_create_index.py
@@ -3,30 +3,7 @@ import pytask
 
 from src.config import BLD
 
-
-def calculate_sub_index_score(df, policy_value, flag, recorded_flag, maximum):
-    """Sub Index Score calculates stringency level for each policy category.
-    Inputs:
-        policy_value (integer)  = ordinal scale measurement that represents level of policy
-                                  stringency.
-        flag (integer)          = dummy variable indicating whether or not an indicator
-                                  uses a flag to distinguish between regionally and federally
-                                  employed policies.
-        recorded_flag (integer) = dummy variable indicating whether the policy is implemented
-                                  regionally or federally.
-        maximum (integer)       = maximum ordinal scale value each indicator can take.
-    For further descriptions on inputs, see code book.
-    Output:
-        Output for calculate_sub_index_score is a value between 0 and 100 with 0 indicating no
-        policy restriction in place and 100 indicating the highest level of stringency a policy
-        can take on.
-    """
-    numerator = df[policy_value] - 0.5 * (df[flag] - df[recorded_flag])
-    denominator = df[maximum]
-    return 100 * (numerator / denominator)
-
-
-new_cols_1 = [
+sub_index_scores = [
     "SI_E1",
     "SI_E2",
     "SI_E3",
@@ -37,62 +14,93 @@ new_cols_1 = [
     "SI_S3",
     "SI_S4",
 ]
-scale_cols = [
-    "scale_E1",
-    "scale_E2",
-    "scale_E3",
-    "scale_E4",
-    "scale_R1",
-    "scale_S1",
-    "scale_S2",
-    "scale_S3",
-    "scale_S4",
-]
-flag_cols = [
-    "flag_E1",
-    "flag_E2",
-    "flag_E3",
-    "flag_E4",
-    "flag_R1",
-    "flag_S1",
-    "flag_S2",
-    "flag_S3",
-    "flag_S4",
-]
-record_cols = [
-    "recorded_flag_E1",
-    "recorded_flag_E2",
-    "recorded_flag_E3",
-    "recorded_flag_E4",
-    "recorded_flag_R1",
-    "recorded_flag_S1",
-    "recorded_flag_S2",
-    "recorded_flag_S3",
-    "recorded_flag_S4",
-]
-max_cols = [
-    "maximum_E1",
-    "maximum_E2",
-    "maximum_E3",
-    "maximum_E4",
-    "maximum_R1",
-    "maximum_S1",
-    "maximum_S2",
-    "maximum_S3",
-    "maximum_S4",
-]
-new_cols_2 = ["E_index_score", "S_index_score", "stringency_index_score"]
-subscore_cols = [new_cols_1[0:4], new_cols_1[5:9], new_cols_1]
+stringency_indices = ["E_index_score", "S_index_score", "stringency_index_score"]
+subscores = [sub_index_scores[0:4], sub_index_scores[5:9], sub_index_scores]
+
+
+def calculate_sub_index_score(df, policy_value, flag, recorded_flag, maximum):
+    """Sub Score Indicies.
+
+    This function calculates stringency level for each policy category.
+
+    Args:
+        df (df): Dataframe
+        policy_value (int): Ordinal scale measurement that represents level of policy
+                            stringency.
+        flag (int): Dummy variable indicating whether or not an indicator
+                    uses a flag to distinguish between regionally and federally
+                    employed policies.
+        recorded_flag (int): Dummy variable indicating whether the policy is implemented
+                             regionally or federally.
+        maximum (int): Maximum ordinal scale value each indicator can take.
+
+    Note:
+        For more detailed descriptions of args, see code book.
+
+    Returns:
+        int: A value between 0 and 100 with 0 indicating no policy restriction in place
+        and 100 indicating the highest level of stringency a policy can take on.
+
+    """
+    numerator = df[policy_value] - 0.5 * (df[flag] - df[recorded_flag])
+    denominator = df[maximum]
+    return 100 * (numerator / denominator)
+
+
+def create_index(
+    df,
+    sub_index_scores,
+    ordinal_values,
+    flag_dummies,
+    recorded_flags,
+    maxima,
+    stringency_indices,
+    subscores,
+    path,
+):
+    """.
+
+    This function calculates.
+
+    Args:
+
+
+
+
+    Returns:
+
+
+    """
+    for sub_index_score, ordinal_value, flag_dummy, recorded_flag, maximum in zip(
+        sub_index_scores, ordinal_values, flag_dummies, recorded_flags, maxima
+    ):
+        df[sub_index_score] = calculate_sub_index_score(
+            df, ordinal_value, flag_dummy, recorded_flag, maximum
+        )
+    for stringency_index, subscore in zip(stringency_indices, subscores):
+        df[stringency_index] = df[subscore].mean(axis=1)
+    return df.to_csv(path)
 
 
 @pytask.mark.depends_on(BLD / "data" / "covid_policy.csv")
 @pytask.mark.produces(BLD / "analysis" / "stringency_index_data.csv")
 def task_create_index(depends_on, produces):
-    stringency_index_data = pd.read_csv(depends_on).set_index("date")
-    for a, b, c, d, e in zip(new_cols_1, scale_cols, flag_cols, record_cols, max_cols):
-        stringency_index_data[a] = calculate_sub_index_score(
-            stringency_index_data, b, c, d, e
-        )
-    for i, j in zip(new_cols_2, subscore_cols):
-        stringency_index_data[i] = stringency_index_data[j].mean(axis=1)
-    stringency_index_data.to_csv(produces)
+    # Read data
+    data = pd.read_csv(depends_on).set_index("date")
+    # Input lists
+    ordinal_values = [col for col in data if col.startswith("scale")]
+    flag_dummies = [col for col in data if col.startswith("flag")]
+    recorded_flags = [col for col in data if col.startswith("recorded")]
+    maxima = [col for col in data if col.startswith("maximum")]
+    # Create dataset with indicies
+    create_index(
+        data,
+        sub_index_scores,
+        ordinal_values,
+        flag_dummies,
+        recorded_flags,
+        maxima,
+        stringency_indices,
+        subscores,
+        produces,
+    )
